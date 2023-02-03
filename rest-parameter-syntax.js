68f8d410296fddea05702edf70a3853c8adedbe7
rpf13 <rpf13@me.com>
2023-02-03 05:51:26 +0000

    feat: Add rest param test

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -5,8 +5,22 @@
  */
 
 // Regular function call 
+const SumAll = (a, b, c) => a + b + c;
+let sum = SumAll(1, 2, 3);
+console.log("Sum:", sum);
 
 // Extra arguments are ignored
+let sum2 = SumAll(1, 2, 3, 4, 5, 6)
+console.log("Sum2:", sum2);
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum;
+}
 
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log("Sum3:", sum3);
