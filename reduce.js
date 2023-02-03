1df2e90f55eed1ef74ea63a9d5465d2383e38f61
rpf13 <rpf13@me.com>
2023-02-03 11:45:21 +0000

    feat: Add reduce site

---
+++ reduce.js
@@ -0,0 +1,48 @@
+// Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
+
+const teamMembers = [
+    {
+      name: 'Andrew',
+      profession: 'Developer',
+      yrsExperience: 5
+    },
+    {
+      name: 'Ariel',
+      profession: 'Developer',
+      yrsExperience: 7
+    },
+    {
+      name: 'Michael',
+      profession: 'Designer',
+      yrsExperience: 1
+    },
+    {
+      name: 'Kelly',
+      profession: 'Designer',
+      yrsExperience: 3
+    }
+  ];
+  
+  // Totaling a specific object property
+
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
+
+  
+  // Grouping by a property, and totaling it too
+
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+    // check if the profession already exists in the accumulator and if not, create it
+    let key = curr.profession;
+    if (!acc[key]) {
+        acc[key] = curr.yrsExperience;
+    } else {
+        acc[key] += curr.yrsExperience;
+    }
+    return acc;
+}, {});
+
+console.log(experienceByProfession);
